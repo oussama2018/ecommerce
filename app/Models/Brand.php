f015ed710dc34e07b9623c8ac5fd0a51ff3d830a
oussama2018 <oussamakmar1@gmail.com>
2025-10-30 23:55:22 +0100

    models and tables

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Brand extends Model
 {
     use HasFactory;
-    protected $fillable=['name','slug','image','is_active'];
+    protected $fillable=['name','image','slug','is_active'];
     public function products(){
         return $this->hasMany(Product::class);
     }
