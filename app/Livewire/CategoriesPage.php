66fda0e449dbde4b902d584af4a42b01f6186ee0
oussama2018 <oussamakmar1@gmail.com>
2025-11-13 00:04:40 +0100

    complete product filters

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -9,7 +9,7 @@ class CategoriesPage extends Component
 {
     public function render()
     {
-        $categories=Category::all();
+        $categories=Category::where('is_active',1)->get();
         return view('livewire.categories-page',[
             'categories'=>$categories
         ]);
---
+++ app/helpers/CartManagement.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\helpers;
+
+class CartManagement{
+    // add item to cart
+
+    // remove item to cart
+
+    // add cart items to cookies
+    // clear cart items from cookie
+
+    // get all cart items from cookies
+
+    // increment item quantity
+
+    // decrement item quantity
+
+    //calculate grand total
+}
